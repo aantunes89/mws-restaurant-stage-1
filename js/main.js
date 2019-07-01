186f397eb48417d13ffdf20f0a57d5eb5c6368cd
André Antunes <andrepalhares1989@gmail.com>
2019-06-30 21:03:25 -0300

    Commit before send to Udacity

--- js/main.js
+++ js/main.js
@@ -164,22 +164,27 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const textContainer = document.createElement('div');
+  textContainer.classList.add('restaurant-text-container')
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  textContainer.append(name)
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  textContainer.append(neighborhood)
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
-
+  textContainer.append(address)
+  
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  textContainer.append(more)
+
+  li.append(textContainer)
 
   return li
 }
